b519434b0c7924e43fe21c69e42ec44508f4a518
trikmbk56 <trikm.bk56@gmail.com>
2015-12-14 09:12:36 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
