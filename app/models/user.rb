5de59d9c72059c602ed56030f5423c3634a0c0ed
trikmbk56 <trikm.bk56@gmail.com>
2015-12-29 10:08:58 +0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  
 	attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 }, 
+	validates :email, presence: true, length: { maximum: 255 },
 		format: { with: VALID_EMAIL_REGEX },
 		uniqueness: { case_sensitive: false }
 	has_secure_password
@@ -66,6 +68,12 @@ class User < ActiveRecord::Base
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   #private function
